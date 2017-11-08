f0e1f6377736e8b07384046fbf7d3e6b362c444e
shin liu <shin820@163.com>
2017-11-08 15:29:01 +0800

    switching to Hybrid Flow

--- IdentityServer4/MvcClient/Controllers/HomeController.cs
+++ IdentityServer4/MvcClient/Controllers/HomeController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using MvcClient.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace MvcClient.Controllers
 {
@@ -23,6 +26,18 @@ namespace MvcClient.Controllers
             await HttpContext.SignOutAsync("Cookies");
             await HttpContext.SignOutAsync("oidc");
             return View("Index");
+        }
+
+        [HttpGet]
+        public async Task<string> CallApiUsingUserAccessToken()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var content = await client.GetStringAsync("http://localhost:5001/identity");
+
+            return content;
         }
 
         public IActionResult Index()
--- IdentityServer4/MvcClient/Startup.cs
+++ IdentityServer4/MvcClient/Startup.cs
@@ -39,7 +39,14 @@ namespace MvcClient
                 options.RequireHttpsMetadata = false;
 
                 options.ClientId = "mvc";
+                options.ClientSecret = "secret";
+                options.ResponseType = "code id_token";
+
                 options.SaveTokens = true;
+                options.GetClaimsFromUserInfoEndpoint = true;
+
+                options.Scope.Add("api1");
+                options.Scope.Add("offline_access");
             });
         }
 
